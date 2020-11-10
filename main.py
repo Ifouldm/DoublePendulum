e78c3e767083096bae9d4ebe89f7cecae7e11269
Matt <ifouldm@gmail.com>
2020-11-10 18:31:26 +0000

    Created Button and Slider

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ width = 600
 height = 600
 
 white = (255, 255, 255)
+blue = (50, 50, 200)
 black = (0, 0, 0)
 
-r1 = 125
+r1 = 100
 r2 = 175
 m1 = 10
-m2 = 4
+m2 = 10
 a1 = 0
 a2 = 0
 a1_v = 0
@@ -21,6 +22,7 @@ cx = 0
 cy = 0
 
 line_buffer = []
+toolbar = []
 
 px2 = -1
 py2 = -1
@@ -29,6 +31,46 @@ pygame.init()
 display = pygame.display.set_mode((width, height))
 
 
+class Button:
+    def __init__(self, x, y, w, h, text, colour):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.text = text
+        self.colour = colour
+
+    def contains(self, pos):
+        return self.x < pos[0] < (self.x + self.w) and self.y < pos[1] < (self.y + self.h)
+
+    def show(self):
+        pygame.draw.rect(display, self.colour, (self.x, self.y, self.w, self.h), 1)
+        text = pygame.font.Font("freesansbold.ttf", 20)
+        text.render(self.text, True, black)
+
+class Slider:
+    def __init__(self, start_val, x, y, w):
+        self.w = w
+        self.y = y
+        self.x = x
+        self.current_val = int(w * start_val)
+        self.h = 20
+
+    def contains(self, pos):
+        return self.x < pos[0] < (self.x + self.w) and self.y < pos[1] < (self.y + self.h)
+
+    def click(self, pos):
+        self.current_val = (pos[0] - self.x) / self.w
+
+    def show(self):
+        pygame.draw.rect(display, blue, (self.x, self.y, self.w, self.h), 1)
+        pygame.draw.circle(display,
+                           blue,
+                           (int(self.x + (self.w * self.current_val)),
+                                int(self.y + self.h / 2)),
+                           int(self.h / 2),
+                           1)
+
 def setup():
     global a1, a2, cx, cy
     a1 = math.pi / 2
@@ -36,6 +78,11 @@ def setup():
     cx = int(width / 2)
     cy = 200
     display.fill(white)
+    clear_button = Button(100, 500, 120, 40, "Clear", blue)
+    toolbar.append(clear_button)
+    slider1 = Slider(0.5, 300, 500, 120)
+    toolbar.append(slider1)
+
 
 
 def translate(x, y):
@@ -45,6 +92,8 @@ def translate(x, y):
 def draw():
     display.fill(white)
     global a2_v, a1_v, a1, a2, line_buffer, px2, py2
+    for item in toolbar:
+        item.show()
     num1 = -g * (2 * m1 + m2) * math.sin(a1)
     num2 = -m2 * g * math.sin(a1 - 2 * a2)
     num3 = -2 * math.sin(a1 - a2) * m2
@@ -68,19 +117,19 @@ def draw():
     pygame.draw.line(display, black, translate(0, 0), translate(x1, y1))
     pygame.draw.circle(display, black, translate(x1, y1), m1)
     pygame.draw.line(display, black, translate(x1, y1), translate(x2, y2))
-    pygame.draw.circle(display, black, translate(x2, y2), m2)
+    pygame.draw.circle(display, blue, translate(x2, y2), m2)
 
     a1_v += a1_a
     a2_v += a2_a
     a1 += a1_v
     a2 += a2_v
 
-    # a1_v *= 0.999
-    # a2_v *= 0.999
+    #a1_v *= 0.999
+    #a2_v *= 0.999
 
     line_buffer.append(translate(x2, y2))
     if px2 != -1:
-        pygame.draw.lines(display, black, False, line_buffer)
+        pygame.draw.lines(display, blue, False, line_buffer, 2)
 
     # buffer.stroke(0)
     # if (frameCount > 1):
@@ -93,6 +142,11 @@ def draw():
 setup()
 while True:
     for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if toolbar[0].contains(pygame.mouse.get_pos()):
+                line_buffer = [line_buffer[-1]]
+            if toolbar[1].contains(pygame.mouse.get_pos()):
+                toolbar[1].click(pygame.mouse.get_pos())
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
